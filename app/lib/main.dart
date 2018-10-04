c9e408e4106f3465e530938c461e52707a795c18
Wagner Nogueira <wagner@ephrom.com.br>
2018-10-04 09:48:19 -0300

    Editando tarefa.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,8 +14,16 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
   List _toDoList = [];
+
   Map<String, dynamic> _lastRemoved;
   int _lastPositionRemoved;
+  bool _estaAdd = true;
+  Map<String, dynamic> _lastUpdated;
+  int _lastPositionUpdated;
+  String _infoBotao = "Inserir";
+  String _infoLabel = "Nova Tarefa";
+  BuildContext _context;
+  
 
   @override
   void initState() {
@@ -34,6 +42,7 @@ class _HomeState extends State<Home> {
     setState(() {
       _toDoController.text = "";
       _toDoList.clear();
+      _saveData();
     });
   }
 
@@ -46,6 +55,45 @@ class _HomeState extends State<Home> {
       _toDoList.add(novaTarefa);
       _saveData();
     });
+  }
+
+  void _addOrUpdateToDo(bool insert) {
+    if (insert) {
+      debugPrint("adicionou");
+      setState(() {
+        Map<String, dynamic> novaTarefa = Map();
+        novaTarefa["title"] = _toDoController.text;
+        novaTarefa["ok"] = false;
+        _toDoController.text = "";
+        _toDoList.add(novaTarefa);
+        _saveData();
+      });
+    } else {
+      setState(() {
+        Map<String, dynamic> tarefaEditada = Map();
+        tarefaEditada["title"] = _toDoController.text;
+        tarefaEditada["ok"] = _lastUpdated["ok"];
+        _toDoController.text = "";
+        _toDoList.insert(_lastPositionUpdated, tarefaEditada);
+        _saveData();
+        
+
+        final snack = SnackBar(
+                content: Text("Tarefa \"${_lastUpdated["title"]}\" editada!"),
+                action: SnackBarAction(
+                    label: "Desfazer",
+                    onPressed: () {
+                      setState(() {
+                        _toDoList.removeAt(_lastPositionUpdated);
+                        _toDoList.insert(_lastPositionUpdated, _lastUpdated);
+                        _saveData();
+                      });
+                    }
+                  ),
+                duration: Duration(seconds: 2));
+            Scaffold.of(_context).showSnackBar(snack);
+      });
+    }
   }
 
   @override
@@ -69,14 +117,16 @@ class _HomeState extends State<Home> {
                   child: TextFormField(
                       controller: _toDoController,
                       decoration: InputDecoration(
-                          labelText: "Nova Tarefa",
+                          labelText: _infoLabel,
                           labelStyle: TextStyle(color: Colors.blueAccent))),
                 ),
                 RaisedButton(
                   color: Colors.blueAccent,
-                  child: Text("Add"),
+                  child: Text(_infoBotao),
                   textColor: Colors.white,
-                  onPressed: _addToDo,
+                  onPressed: () {
+                    _addOrUpdateToDo(_estaAdd);
+                  },
                 ),
               ],
             ),
@@ -102,7 +152,7 @@ class _HomeState extends State<Home> {
           color: Colors.green,
           child: Align(
             alignment: Alignment(0.9, 0.0),
-            child: Icon(Icons.add, color: Colors.white),
+            child: Icon(Icons.mode_edit, color: Colors.white),
           )),
       //direction: DismissDirection.startToEnd,
       background: Container(
@@ -124,25 +174,41 @@ class _HomeState extends State<Home> {
         },
       ),
       onDismissed: (direction) {
-        setState(() {
-          _lastRemoved = Map.from(_toDoList[index]);
-          _lastPositionRemoved = index;
-          _toDoList.removeAt(index);
-          _saveData();
+        // Esquerda pra direita remover
+        if (direction == DismissDirection.startToEnd) {
+          setState(() {
+            _lastRemoved = Map.from(_toDoList[index]);
+            _lastPositionRemoved = index;
+            _toDoList.removeAt(index);
+            _saveData();
 
-          final snack = SnackBar(
-              content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
-              action: SnackBarAction(
-                  label: "Desfazer",
-                  onPressed: () {
-                    setState(() {
-                      _toDoList.insert(_lastPositionRemoved, _lastRemoved);
-                      _saveData();
-                    });
-                  }),
-              duration: Duration(seconds: 2));
-          Scaffold.of(context).showSnackBar(snack);
-        });
+            final snack = SnackBar(
+                content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+                action: SnackBarAction(
+                    label: "Desfazer",
+                    onPressed: () {
+                      setState(() {
+                        _toDoList.insert(_lastPositionRemoved, _lastRemoved);
+                        _saveData();
+                      });
+                    }),
+                duration: Duration(seconds: 2));
+            Scaffold.of(context).showSnackBar(snack);
+          });
+        }
+        // Direita pra esquerda editar
+        else if (direction == DismissDirection.endToStart) {
+          setState(() {
+            _lastUpdated = Map.from(_toDoList[index]);
+            _lastPositionUpdated = index;
+            _toDoController.text = _lastUpdated["title"];
+            _infoBotao = "Salvar";
+            _infoLabel = "Editar Tarefa";
+            _estaAdd = false;
+            _toDoList.removeAt(index);
+            _context = context;
+          });
+        }
       },
     );
   }
